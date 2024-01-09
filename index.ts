35400994394461c5824596247f9685dee6ad9161
dennyblack11 <dennisozoemena3@gmail.com>
2024-01-09 17:45:06 +0100

    adding mainApp to production

--- index.ts
+++ index.ts
@@ -1,30 +1,32 @@
-import express,{Application} from "express";
+import express, { Application } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { dbConfig } from "./utils/dbConfig";
+import { mainApp } from "./mainApp";
 
 dotenv.config();
 
-const port:number = parseInt(process.env.PORT!);
-const app:Application = express();
+const port: number = parseInt(process.env.PORT!);
+const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 
-const server = app.listen(port, ()=>{
-    console.clear();
-    console.log("first");
-    dbConfig()
+mainApp(app);
+const server = app.listen(port, () => {
+  console.clear();
+  console.log("first");
+  dbConfig();
 });
 
-process.on("uncaughtException",(err:Error)=>{
-    console.log("uncaughtException",err);
+process.on("uncaughtException", (err: Error) => {
+  console.log("uncaughtException", err);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason: any) => {
+  console.log("unhandledRejection", reason);
+  server.close(() => {
     process.exit(1);
-});
-
-process.on("unhandledRejection", (reason:any)=>{
-    console.log("unhandledRejection", reason);
-    server.close(()=>{
-        process.exit(1);
-    });
+  });
 });
